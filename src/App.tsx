67db704140c1cc220402929fba8d9597418cc205
eliasroodrigues <elias.roodrigues98@gmail.com>
2022-05-02 13:23:14 -0300

    widget button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-interface ButtonProps {
-  text?: string;
-}
+import { Widget } from './components/Widget';
 
-function Button(props: ButtonProps) {
-  return <button className="bg-violet-500 p-2">{props.text ?? 'Enviar'}</button>
+export function App() {
+  return <Widget />
 }
-
-function App() {
-  return (
-    <div>
-      <Button />
-      <Button text="Reset" />
-    </div>
-  )
-}
-
-export default App
